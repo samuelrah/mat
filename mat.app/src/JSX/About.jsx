25d750d0305cc11848fd6988dbf5c2651b3dfef3
samuelrah <samuel.rahseparmohammadi@elev.ga.ntig.se>
2026-05-06 16:02:38 +0200

    about

--- mat.app/src/JSX/About.jsx
+++ mat.app/src/JSX/About.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import VerticalSlideshow from './VerticalSlideshow';
+import React from "react";
 
 export default function About() {
   return (
-    <div className="about-page">
-      <VerticalSlideshow />
+    <div className="about-page d-flex flex-column justify-content-center align-items-center" style={{height: '100vh', color: 'white'}}>
+      <div className="text-center" style={{marginTop: 100}}>
+        <h5>Om oss</h5>
+        <p style={{maxWidth: 400}}>
+          Här är information om våran resturang, detta är en resturang, det finns massor av mat och drickor och annat type shi.
+        </p>
+      </div>
+      <div className="w-100 d-flex justify-content-between align-items-end" style={{position: 'absolute', bottom: 10, left: 0, padding: '0 20px', fontSize: 17}}>
+        <div>
+          kontakt information:<br/>
+          <a href="samuel.rahseparmohammadi@elev.ga.ntig.se">exempel@gmail.com</a> Telefonnummer: 1177
+        </div>
+        <div>
+          Address: Kronhusgatan
+        </div>
+      </div>
     </div>
   );
 }
