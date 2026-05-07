97e78d37ebed5f7036b7680ac3791b323dc0b5c4
Skibidijonathan <Jonathan.jadebondkoivumaa@elev.ga.ntig.se>
2026-05-07 08:58:26 +0200

    småfixar
    sök funktionen i mitten av skärmen samt beställ nu knappen på startsidan tar dig till menyn

--- mat.app/src/JSX/Home.jsx
+++ mat.app/src/JSX/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="content text-center">
-      <button className="btn btn-dark">beställ nu</button>
+      <button className="btn btn-dark" onClick={() => navigate("/menu")}>beställ nu</button>
     </div>
   );
 }
